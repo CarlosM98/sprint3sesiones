5500857c394ad3022a70ae8c7f62818b79ff87fa
Carlos <jmiguenss24@fpcoruna.afundacion.org>
2025-10-29 12:33:56 +0100

    Ejercicio 7

--- main.php
+++ main.php
@@ -14,32 +14,53 @@
 				height: 150px;
 			}
 			a{ text-decoration: none;}
+
+			li{
+				text-decoration: none;
+			}
+			ul{
+				width: 50vw;
+				margin: auto;
+				padding: 30px;
+			}
+
+			li{
+				width: 25vw;
+				margin: auto;
+				transition: all 0.4s ease-in-out;
+			}
+			li:hover{
+				transform: scale(1.03);
+				background-color: gray;
+			}
 		</style>
 	</head>
         <body>
-                <?php
-                $query = 'select * from tLibros';
+             <?php
+            	$query = 'select * from tLibros';
                 $result =  mysqli_query($db, $query) or die('Query error');
-		echo 'Numero de filas: '.mysqli_num_rows($result).'<br>';
-                while($row = mysqli_fetch_array($result)){
-			echo $row['id'];
-			echo '<br>';
-			echo $row['nombre'];
-			echo '<br>';
-                        echo '<img src="'.$row['url_imagen'].'" alt="Imagen libro">';
-                        echo '<br>';
-                        echo $row['autor'];
-                        echo '<br>';
-                        echo $row['anho_publicacion'];
-                        echo '<br>';
-			echo '<p><a href="/detail.php?id='.$row['id'].'">Ver detalles</a></p>';
-			echo '<br>';
-			echo '<hr>';
-			echo '<br>';
+				echo 'Numero de filas: '.mysqli_num_rows($result).'<br>';
+				echo '<ul>';
+				while($row = mysqli_fetch_array($result)){
+					echo '<li">';
+					echo $row['id'];
+					echo '<br>';
+					echo $row['nombre'];
+					echo '<br>';
+					echo '<img src="'.$row['url_imagen'].'" alt="Imagen libro">';
+					echo '<br>';
+					echo $row['autor'];
+					echo '<br>';
+					echo $row['anho_publicacion'];
+					echo '<br>';
+					echo '<p><a href="/detail.php?id='.$row['id'].'">Ver detalles</a></p>';
+					echo '</li>';
+					echo '<hr>';
 
-		}
-		?>
-		<a href="/logout.php">Logout</a>
+				}
+				echo '</ul>';
+			?>
+			<a href="/logout.php">Logout</a>
 	
 	</body>
 </html>
